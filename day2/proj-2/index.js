58d7f756e11e7d0218d43ebb289ec3a17b403e4f
Chris Lee <finges@gmail.com>
2016-10-15 17:21:36 -0700

    Completed project 2

--- day2/proj-2/index.js
+++ day2/proj-2/index.js
@@ -5,8 +5,9 @@
 //** Part 1
 //**
 //**********************************************
-//**
-//**
+//** 
+//** Change Log
+//** 10/15/16 - Add Project Part 2 - ChrisL
 //**
 //**
 //**
@@ -15,8 +16,31 @@
 //**
 //**********************************************
 
+var firstName;
+var lastName;
+var battlefieldWidth;
+var boardRow = "";
+
 // Ask the player for their name and store the answer
-var firstName = prompt("Please Enter Your First Name");
-var lastName = prompt("Please Enter Your Last Name");
-// Greet the player by name
-alert("Welcome " + firstName + " " + lastName + " to the Game!");
+firstName = prompt("Please Enter Your First Name");
+lastName = prompt("Please Enter Your Last Name");
+
+// Get board width, loop until entry is valid.
+while (battlefieldWidth < 5 || battlefieldWidth > 20 || battlefieldWidth == undefined) {
+  if (battlefieldWidth == undefined) {
+    battlefieldWidth = prompt("How wide do would you like your battlefield? (5-20)");
+  } else {
+    battlefieldWidth = prompt('Your battlefield width has to be between 5 and 20, Please try again');
+  }
+}
+
+// Calculate what a row of the board looks like
+for (i = 0; i < battlefieldWidth; i++) {
+ boardRow = "w" + boardRow;
+}
+
+// Personalize Greeting on page
+document.getElementById("title").innerHTML = "Welcome " + firstName + " " + lastName + " to the Game!";
+
+// Display Board
+document.getElementById("board").innerHTML = boardRow;
